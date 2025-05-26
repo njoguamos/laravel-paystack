d29c6f97dd1429cd500650f1e1d49d3662d84920
Njogu Amos <29255728+njoguamos@users.noreply.github.com>
2025-05-26 06:06:40 +0300

    refacfor: rename data objects

--- tests/Feature/TransactionTest.php
+++ tests/Feature/TransactionTest.php
@@ -6,8 +6,8 @@ use Saloon\Http\Faking\MockClient;
 use Saloon\Http\Faking\MockResponse;
 use NjoguAmos\Paystack\Facades\Transaction;
 use NjoguAmos\Paystack\Requests\Transactions\InitializeTransaction;
-use NjoguAmos\Paystack\Data\Transactions\TransactionInitRequestData;
-use NjoguAmos\Paystack\Data\Transactions\TransactionInitResponseData;
+use NjoguAmos\Paystack\Data\Transactions\InitializeRequestData;
+use NjoguAmos\Paystack\Data\Transactions\InitializeResponseData;
 
 test(description: 'can initialise a transaction', closure: function () {
     $body = [
@@ -23,11 +23,11 @@ test(description: 'can initialise a transaction', closure: function () {
     /** @noinspection PhpParamsInspection */
     MockClient::global(mockData: [InitializeTransaction::class => MockResponse::make(body: $body) ]);
 
-    $data = new TransactionInitRequestData(amount: 1000, email: 'customer@example.com');
+    $data = new InitializeRequestData(amount: 1000, email: 'customer@example.com');
     $response = Transaction::initialize(data: $data);
 
     expect(value: $response)
-        ->toBeInstanceOf(class: TransactionInitResponseData::class)
+        ->toBeInstanceOf(class: InitializeResponseData::class)
         ->authorization_url->toBe(expected: $body['data']['authorization_url'])
         ->access_code->toBe(expected: $body['data']['access_code'])
         ->reference->toBe(expected: $body['data']['reference']);
